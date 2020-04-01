2b331cfbdc05f9f9635db4f4987c6d1d7c15bd33
hgko1207 <khkkhk1207@gmail.com>
2020-04-01 10:36:30 +0900

    signup state and custom hook

--- ch1/front/pages/signup.js
+++ ch1/front/pages/signup.js
@@ -4,16 +4,23 @@ import AppLayout from "../components/AppLayout";
 import { Form, Input, Checkbox, Button } from "antd";
 
 const Signup = () => {
-  const [id, setId] = useState("");
   const [nick, setNick] = useState("");
   const [password, setPassword] = useState("");
   const [passwordCheck, setPasswordCheck] = useState("");
   const [term, setTerm] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [termError, setTermError] = useState(false);
 
-  const onSubmit = () => {};
-  const onChangeId = e => {
-    setId(e.target.value);
+  const onSubmit = e => {
+    e.preventDefault();
+    if (password !== passwordCheck) {
+      return setPasswordError(true);
+    }
+    if (!term) {
+      return setTermError(true);
+    }
   };
+
   const onChangeNick = e => {
     setNick(e.target.value);
   };
@@ -21,11 +28,23 @@ const Signup = () => {
     setPassword(e.target.value);
   };
   const onChangePasswordCheck = e => {
+    setPasswordError(e.target.value !== password);
     setPasswordCheck(e.target.value);
   };
   const onChangeTerm = e => {
-    setTerm(e.target.value);
+    setTermError(e.target.checked);
+    setTerm(e.target.checked);
   };
+
+  const useInput = (initValue = null) => {
+    const [value, setter] = useState(initValue);
+    const handler = e => {
+      setter(e.target.value);
+    };
+    return [value, handler];
+  };
+
+  const [id, onChangeId] = useInput("");
 
   return (
     <>
@@ -34,7 +53,7 @@ const Signup = () => {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.0.4/antd.css" />
       </Head>
       <AppLayout>
-        <Form onSubmit={onSubmit} style={{ padding: 10 }}>
+        <Form onSubmitCapture={onSubmit} style={{ padding: 15 }}>
           <div>
             <label htmlFor="uesr-id">아이디</label>
             <br />
@@ -66,15 +85,17 @@ const Signup = () => {
               required
               onChange={onChangePasswordCheck}
             />
+            {passwordError && <div style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</div>}
           </div>
           <div>
-            <Checkbox name="user-term" value={term} onChange={onChangeTerm}>
+            <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
               정보제공에 동의합니다.
             </Checkbox>
+            {termError && <div style={{ color: "red" }}>약관에 동의하셔야 합니다.</div>}
           </div>
-          <div>
+          <div style={{ marginTop: 10 }}>
             <Button type="primary" htmlType="submit">
-              회원가입
+              가입하기
             </Button>
           </div>
         </Form>
