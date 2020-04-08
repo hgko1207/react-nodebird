6711428382897c347696448b91ef3c0a3b4f1479
hgko1207 <khkkhk1207@gmail.com>
2020-04-08 14:55:44 +0900

    react-redux connect

--- ch1/front/pages/index.js
+++ ch1/front/pages/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import PostForm from "../components/PostForm";
 import PostCard from "../components/PostCard";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector, connect } from "react-redux";
 import { loginAction } from "../reducers/user";
 
 const dummy = {
@@ -19,6 +19,7 @@ const dummy = {
   ],
 };
 
+// hook 방식
 const Home = () => {
   const dispatch = useDispatch();
   const { isLoggedIn, user } = useSelector((state) => state.user);
@@ -38,3 +39,35 @@ const Home = () => {
 };
 
 export default Home;
+
+// high order component 방식
+// const Home = ({ data, dispatch, login }) => {
+//   const { user } = data;
+//   useEffect(() => {
+//     login();
+//   }, []);
+
+//   return (
+//     <div>
+//       {user ? <div>로그인 했습니다: {user.nickname}</div> : <div>로그아웃 했습니다.</div>}
+//       {dummy.isLoggedIn && <PostForm />}
+//       {dummy.mainPosts.map((c) => {
+//         return <PostCard key={c} post={c} />;
+//       })}
+//     </div>
+//   );
+// };
+
+// function mapStateToProps(state) {
+//   return {
+//     data: state.user,
+//   };
+// }
+
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     login: () => dispatch(loginAction),
+//   };
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Home);
