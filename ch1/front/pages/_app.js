5b4b706e6cfcb80844a5db9bad410891c19d292f
hgko1207 <khkkhk1207@gmail.com>
2020-04-28 17:58:03 +0900

    saga-middleware connect

--- ch1/front/pages/_app.js
+++ ch1/front/pages/_app.js
@@ -1,11 +1,14 @@
-import React from "react";
-import Head from "next/head";
-import PropTypes from "prop-types";
-import withRedux from "next-redux-wrapper";
-import AppLayout from "../components/AppLayout";
-import { createStore, compose, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import reducer from "../reducers";
+import React from 'react';
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+import withRedux from 'next-redux-wrapper';
+import AppLayout from '../components/AppLayout';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+import createSagaMiddleware from 'redux-saga';
+import reducer from '../reducers';
+import rootSaga from '../sagas';
 
 const NodeBird = ({ Component, store }) => {
   return (
@@ -23,20 +26,39 @@ const NodeBird = ({ Component, store }) => {
 
 NodeBird.propTypes = {
   Component: PropTypes.elementType,
-  store: PropTypes.object
+  store: PropTypes.object,
 };
+
+// custom middleware
+// const middleware = (store) => (next) => (action) => {
+//   console.log(action); // 다른 작업들을 여기에
+//   next(action);
+// };
+
+// hoc(high order component)
+// connect(mapStateToProps)(Component);
+// const connect = (mapStateToProps) => (Compoment) => () => {
+//   console.log("I'm hoc");
+//   return <Component props={mapStateToProps()} />;
+// };
 
 export default withRedux((initialState, options) => {
   // 여기에다가 store 커스터마이징
-  const middlewares = [];
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
 
-  const enhancer = compose(
-    applyMiddleware(...middlewares),
-    !options.isServer && window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
-      ? window.__REDUX_DEVTOOLS_EXTENSION__()
-      : f => f
-  );
+  // 개발 중일 때만 redux devtools 사용할 수 있도록
+  const enhancer =
+    process.env.NODE_ENV === 'production'
+      ? compose(applyMiddleware(...middlewares))
+      : compose(
+          applyMiddleware(...middlewares),
+          !options.isServer && typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined'
+            ? window.__REDUX_DEVTOOLS_EXTENSION__()
+            : (f) => f
+        );
 
   const store = createStore(reducer, initialState, enhancer);
+  sagaMiddleware.run(rootSaga);
   return store;
 })(NodeBird);
